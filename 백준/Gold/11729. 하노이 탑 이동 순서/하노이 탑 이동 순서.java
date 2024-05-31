8cfff48c7c3080774edc138bf031365d095e73fa
aren-aren <152937401+aren-aren@users.noreply.github.com>
2024-05-31 17:30:51 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 560 ms, Memory: 40580 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main{
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextByte();
+
+        System.out.println((1<<n) -1);
+        StringBuilder sb = new StringBuilder();
+        move(1,3,n, sb);
+        System.out.println(sb);
+    }
+
+    public static void move(int from, int to, int n, StringBuilder sb){
+        if(n == 0) return;
+
+        int other = 6 - from - to;
+
+        move(from, other, n-1, sb);
+        sb.append(from).append(" ").append(to).append("\n");
+        move(other, to, n-1, sb);
+    }
+}
